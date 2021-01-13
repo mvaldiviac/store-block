a02d9f84d99c949997a5543218c210bc0cd5817e
Miguel <m.valdivia.c@gmail.com>
2021-01-13 12:49:03 -0300

    ajustes

--- react/Countdown.tsx
+++ react/Countdown.tsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useState }  from 'react'
+import { TimeSplit } from './typings/global'
+import { tick, getTwoDaysFromNow } from './utils/time'
+import { useCssHandles } from 'vtex.css-handles'
 
-interface CountdownProps {}
+interface CountdownProps {
+  targetDate: string
+}
 
-const Countdown: StorefrontFunctionComponent<CountdownProps> = ({}) => {
+const DEFAULT_TARGET_DATE = getTwoDaysFromNow()
+
+const CSS_HANDLES = ['countdown']
+
+const Countdown: StorefrontFunctionComponent<CountdownProps> = ({ targetDate = DEFAULT_TARGET_DATE }) => {
+  const [timeRemaining, setTime] = useState<TimeSplit>({
+    hours: '00',
+    minutes: '00',
+    seconds: '00'
+  })
+
+  const handles = useCssHandles(CSS_HANDLES)
+  
+  tick(targetDate, setTime)
+  
   return (
-    <div> 
-    <h1>Countdown Prueba App</h1>
+    <div className={`${handles.countdown} c-muted-1 db tc`}> 
+      { `${timeRemaining.hours}:${timeRemaining.minutes}:${timeRemaining.seconds}` }
   </div>
   )
 }
@@ -14,7 +33,14 @@ Countdown.schema = {
   title: 'editor.countdown.title',
   description: 'editor.countdown.description',
   type: 'object',
-  properties: {},
+  properties: {
+    targetDate: {
+      title: 'Final date',
+      description: 'Finaldate used in the countdown',
+      type: 'string',
+      default: null
+    },
+  },
 }
 
 export default Countdown
